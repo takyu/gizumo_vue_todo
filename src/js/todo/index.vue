62b0810fb209bffa495f58dca716f4c7727ade42
takyu <takyu613.study@gmail.com>
2022-06-01 09:43:16 +0900

    :sparkles: create todo list (by one page)

--- src/js/todo/index.vue
+++ src/js/todo/index.vue
@@ -6,73 +6,263 @@
       </header>
 
       <main class="main">
-        <form class="register">
+        <form
+          class="register"
+          @submit.prevent="targetTodo.id ? editTodo() : addTodo()"
+        >
           <div class="register__input">
             <p class="register__input__title">やることのタイトル</p>
             <input
+              v-model="targetTodo.title"
               type="text"
               name="title"
               placeholder="ここにTODOのタイトルを記入してください"
-            >
+              required
+            />
           </div>
           <div class="register__input">
             <p class="register__input__title">やることの内容</p>
             <textarea
+              v-model="targetTodo.detail"
               name="detail"
               rows="3"
               placeholder="ここにTODOの内容を記入してください。改行は半角スペースに変換されます。"
+              required
             />
           </div>
           <div class="register__submit">
             <button class="register__submit__btn" type="submit" name="button">
-              登録する
+              <template v-if="targetTodo.id">
+                <span>変更する</span>
+              </template>
+              <template v-else>
+                <span>登録する</span>
+              </template>
             </button>
           </div>
         </form>
 
+        <div v-if="errorMessage" class="error">
+          <p class="error__text">{{ errorMessage }}</p>
+        </div>
+
         <div class="todos">
-          <ul class="todos__list">
-            <li>
-              <div class="todos__inner">
-                <div class="todos__completed">
-                  <button class="todos__completed__btn" type="button">未完了</button>
+          <template v-if="todos.length">
+            <ul class="todos__list">
+              <li
+                v-for="todo in todos"
+                :key="todo.id"
+                :class="todo.completed ? 'is-completed' : ''"
+              >
+                <div class="todos__inner">
+                  <div class="todos__completed">
+                    <button
+                      class="todos__completed__btn"
+                      type="button"
+                      @click="changeCompleted(todo)"
+                    >
+                      <template v-if="todo.completed">
+                        <span>完了</span>
+                      </template>
+                      <template v-else>
+                        <span>未完了</span>
+                      </template>
+                    </button>
+                  </div>
+                  <div class="todos__desc">
+                    <h2 class="todos__desc__title">
+                      {{ todo.title }}
+                    </h2>
+                    <p class="todos__desc__detail">
+                      {{ todo.detail }}
+                    </p>
+                  </div>
+                  <div class="todos__btn">
+                    <button
+                      class="todos__btn__edit"
+                      type="button"
+                      @click="showEditor(todo)"
+                    >
+                      編集
+                    </button>
+                    <button
+                      class="todos__btn__delete"
+                      type="button"
+                      @click="deleteTodo(todo.id)"
+                    >
+                      削除
+                    </button>
+                  </div>
                 </div>
-                <div class="todos__desc">
-                  <h2 class="todos__desc__title">ここにはTodoのタイトルが入ります</h2>
-                  <p class="todos__desc__detail">ここにはTodoの内容が入ります</p>
-                </div>
-                <div class="todos__btn">
-                  <button class="todos__btn__edit" type="button">編集</button>
-                  <button class="todos__btn__delete" type="button">削除</button>
-                </div>
-              </div>
-            </li>
-          </ul>
+              </li>
+            </ul>
+          </template>
+
+          <template v-else>
+            <p class="todos__empty">
+              やることリストには何も登録されていません。
+            </p>
+          </template>
         </div>
       </main>
 
       <footer class="footer">
         <p>全項目数: 0</p>
-        <p>完了済: 0</p>
-        <p>未完了: 0</p>
+        <p>完了済: {{ todos.filter(todo => todo.completed).length }}</p>
+        <p>未完了: {{ todos.filter(todo => !todo.completed).length }}</p>
       </footer>
     </div>
   </div>
 </template>
 
 <script>
+import axios from 'axios';
+
 export default {
   data() {
     return {
-      todos: [
-        // {
-        //   id: 1,
-        //   title: 'タイトル 01',
-        //   detail: '詳細 01',
-        //   completed: false,
-        // },
-      ],
+      todos: [],
+      targetTodo: {
+        id: null,
+        title: '',
+        detail: '',
+        completed: false,
+      },
+      errorMessage: '',
     };
+  },
+  created() {
+    axios
+      .get('http://localhost:3000/api/todos/')
+      .then(({ data }) => {
+        this.todos = data.todos.reverse();
+      })
+      .catch((err) => {
+        if (err.response) {
+          this.errorMessage = err.response.data.message;
+        } else {
+          this.errorMessage = 'ネットに接続がされていない、もしくはサーバーとの接続がされていません。ご確認ください。';
+        }
+      });
+  },
+  methods: {
+
+    /**
+     * common function
+     */
+    initTargetTodo() {
+      return {
+        id: null,
+        title: '',
+        detail: '',
+        completed: false,
+      };
+    },
+    hideError() {
+      this.errorMessage = '';
+    },
+    showError(err) {
+      if (err.response) {
+        this.errorMessage = err.response.data.message;
+      } else {
+        this.errorMessage = 'ネットに接続がされていない、もしくはサーバーとの接続がされていません。ご確認ください。';
+      }
+    },
+
+    /**
+     * CRUD function
+     */
+    addTodo() {
+      const postTodo = Object.assign({}, {
+        title: this.targetTodo.title,
+        detail: this.targetTodo.detail,
+      });
+      axios.post('http://localhost:3000/api/todos/', postTodo).then(({ data }) => {
+        this.todos.unshift(data);
+        this.targetTodo = Object.assign({}, this.targetTodo, { title: '', detail: '' });
+
+        // delete before error.
+        this.hideError();
+      }).catch((err) => {
+        this.showError(err);
+      });
+    },
+
+    changeCompleted(todo) {
+      // Initialize targetTodo
+      this.targetTodo = this.initTargetTodo();
+
+      const targetTodo = Object.assign({}, todo);
+      axios.patch(`http://localhost:3000/api/todos/${targetTodo.id}`, {
+        completed: !targetTodo.completed,
+      }).then(({ data }) => {
+        this.todos = this.todos.map((todoItem) => {
+          if (todoItem.id === targetTodo.id) return data;
+          return todoItem;
+        });
+
+        // delete before error.
+        this.hideError();
+      }).catch((err) => {
+        this.showError(err);
+      });
+    },
+
+    showEditor(todo) {
+      this.targetTodo = Object.assign({}, todo);
+    },
+
+    editTodo() {
+      // action when is duplicated title and detail
+      const targetTodo = this.todos.find(todo => todo.id === this.targetTodo.id);
+      if (
+        targetTodo.title === this.targetTodo.title
+        && targetTodo.detail === this.targetTodo.detail
+      ) {
+        this.targetTodo = {
+          id: null,
+          title: '',
+          detail: '',
+          completed: false,
+        };
+        return;
+      }
+
+      axios.patch(`http://localhost:3000/api/todos/${this.targetTodo.id}`, {
+        title: this.targetTodo.title,
+        detail: this.targetTodo.detail,
+      }).then(({ data }) => {
+        this.todos = this.todos.map((todo) => {
+          if (todo.id === this.targetTodo.id) return data;
+          return todo;
+        });
+        this.targetTodo = {
+          id: null,
+          title: '',
+          detail: '',
+          completed: false,
+        };
+
+        // delete before error.
+        this.hideError();
+      }).catch((err) => {
+        this.showError(err);
+      });
+    },
+
+    deleteTodo(id) {
+      // Initialize targetTodo
+      this.targetTodo = this.initTargetTodo();
+
+      axios.delete(`http://localhost:3000/api/todos/${id}`).then(({ data }) => {
+        this.todos = data.todos.reverse();
+
+        // delete before error.
+        this.hideError();
+      }).catch((err) => {
+        this.showError(err);
+      });
+    },
   },
 };
 </script>
@@ -129,7 +319,8 @@ export default {
       font-weight: bold;
       font-size: 14px;
     }
-    input, textarea {
+    input,
+    textarea {
       padding: 10px;
       width: 100%;
       font-size: 14px;
@@ -172,7 +363,7 @@ export default {
     & > li {
       padding: 15px 10px;
       border-top: 1px solid #ddd;
-      transition: all .3s;
+      transition: all 0.3s;
       &:first-child {
         border-top: none;
       }
@@ -208,7 +399,7 @@ export default {
       border-radius: 7px;
       border: 2px solid #ff1919;
       background-color: #fff;
-      transition: all .3s;
+      transition: all 0.3s;
     }
   }
   &__desc {
@@ -219,14 +410,14 @@ export default {
       font-weight: bold;
       font-size: 16px;
       line-height: 1.2;
-      transition: all .3s;
+      transition: all 0.3s;
       input {
         padding: 5px 10px;
         width: 100%;
         color: #000;
         font-size: 16px;
         border: 1px solid #ddd;
-        transition: all .3s;
+        transition: all 0.3s;
       }
     }
     &__detail {
@@ -234,14 +425,14 @@ export default {
       color: #777;
       font-size: 14px;
       line-height: 1.2;
-      transition: all .3s;
+      transition: all 0.3s;
       textarea {
         padding: 5px 10px;
         width: 100%;
         color: #000;
         font-size: 14px;
         border: 1px solid #ddd;
-        transition: all .3s;
+        transition: all 0.3s;
       }
     }
   }
@@ -257,7 +448,7 @@ export default {
       font-size: 12px;
     }
     &__edit {
-      background-color: #07C4D7;
+      background-color: #07c4d7;
     }
     &__delete {
       margin-top: 5px;
